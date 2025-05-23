abba62fde6d69c4d222e944a1dbf71b4a1c99fb0
Shayden seymour <shaydenseymour@gmail.com>
2025-05-23 22:27:01 +1200

    Shutdown function

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+void shutdown(const int& code) noexcept {
+  exit(code);
+}
+
 int main() {
   std::cout << "Hello, World!" << std::endl;
   return 0;
--- src/include/Main.h
+++ src/include/Main.h
@@ -1,3 +1,9 @@
 #pragma once
 
 #define UNREACHABLE __builtin_unreachable()
+
+/**
+ * @brief Shutdowns sockets, and the server
+ * @param code Status code to exit server with
+ */
+void shutdown(const int& code) noexcept;
